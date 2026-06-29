7e36565fb97b17156f20f2099f87971e9eb7e2cc
iqlavya <anilstheory@gmail.com>
2026-06-29 11:59:08 +0530

    change to d

--- api/order-lookup.js
+++ api/order-lookup.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'https://medallion-9178.myshopify.com');
+  res.setHeader('Access-Control-Allow-Origin', 'prabhjeetsingh1490.myshopify.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   const { order_id } = req.query;
